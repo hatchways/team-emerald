171ca96d6c9cbb540b9c879ad5885d29d1b12406
Simon Mai <maismin@gmail.com>
2019-11-15 17:58:01 -0500

    Remove initial files and packages from /server

--- server/routes/index.js
+++
@@ -1,8 +0,0 @@
-var express = require("express");
-var router = express.Router();
-
-router.get("/welcome", function(req, res, next) {
-  res.status(200).send({ welcomeMessage: "Step 1 (completed)" });
-});
-
-module.exports = router;
