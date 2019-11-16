98dc29ed489e102b3ae83ec75d79c9d932c09d2f
Simon Mai <maismin@gmail.com>
2019-11-15 21:59:49 -0500

    Add express middleware
    - Add helmet to set various HTTP headers for security
    - Add cors to enable cross-origin resource sharing
    - Add xss-clean to sanitize user input from POST body, GET queries, and
    URL params
    - Add express-rate-limit to limit repeated requests to APIs
    - Add cookie-parser to access cookies
    - Add hpp to prevent HTTP parameter pollution attacks
    - Add express-mongo-sanitize to sanitize user input and prevent MongoDB
    operator injection

--- server/app/app.js
+++ server/app/app.js
@@ -1,6 +1,13 @@
 const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
+const xss = require('xss-clean');
+const rateLimit = require('express-rate-limit');
+const cookiePaser = require('cookie-parser');
+const hpp = require('hpp');
+const mongoSanitize = require('express-mongo-sanitize');
 
 const connectDB = require('./utils/database');
 const errorHandler = require('./middleware/error');
@@ -19,6 +26,31 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+// Set security headers
+app.use(helmet());
+
+// Enable CORS
+app.use(cors());
+
+// Prevent XSS attacks
+app.use(xss());
+
+// Rate limiting
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 10 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+});
+app.use(limiter);
+
+// Cookie parser
+app.use(cookiePaser());
+
+// Prevent HTTP parameter pollution
+app.use(hpp());
+
+// Sanitize data
+app.use(mongoSanitize());
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
