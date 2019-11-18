122651900507b1039380270250fc75f9b6d3c64d
Simon Mai <maismin@gmail.com>
2019-11-18 01:17:04 -0500

    Refactor frontend setup
    - Add prettier dependency and its config file
    - Fix eslint config to use airbnb's linting rules
    - Add husky and lint-staged to lint and format code before committing
    - Ran prettier on existing files to ensure consistency in code styles
    - Add proptypes dependency and fix files without proptypes
    - Fix files with react/jsx lint errors

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
-import React from "react";
-import { MuiThemeProvider } from "@material-ui/core";
-import { BrowserRouter, Route } from "react-router-dom";
+import React from 'react';
+import { MuiThemeProvider } from '@material-ui/core';
+import { BrowserRouter, Route } from 'react-router-dom';
 
-import { theme } from "./themes/theme";
-import LandingPage from "./pages/Landing";
+import theme from './themes/theme';
+import LandingPage from './pages/Landing';
 
-import "./App.css";
+import './App.css';
 
 function App() {
   return (
