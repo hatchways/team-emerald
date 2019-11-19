76b25a83d11766b684da9dbce73ccdff4f9f2022
Simon Mai <maismin@gmail.com>
2019-11-18 22:12:42 -0500

    Fix documentation
    - Add periods to sentences in documentation

--- server/app/middleware/error.js
+++ server/app/middleware/error.js
@@ -2,12 +2,12 @@ require('colors');
 const ErrorResponse = require('../utils/errorResponse');
 
 /**
- * The next middleware function in the stack
+ * The next middleware function in the stack.
  * @callback nextCallback
  */
 
 /**
- * Returns the response object with an error message and status code
+ * Returns the response object with an error message and status code.
  * @param {Error} err - the error object
  * @param {object} req - the request object
  * @param {object} res - the response object
