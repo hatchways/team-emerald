62973269d773bb037c0165abee2291ad31c511b9
Erik Soderholm <esodey@gmail.com>
2019-11-20 14:27:13 -0800

    "Small textfield changes. Starting to setup for routing"

--- client/src/pages/signInForm.js
+++ client/src/pages/signInForm.js
@@ -36,13 +36,88 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
-export default function SignIn() {
+function Redirect() {
+  return <h1>Dashboard</h1>;
+}
+
+function Loading() {
+  return <p>Loading...</p>;
+}
+
+function registerReducer(state, action) {
+  if (action.type === 'login') {
+    return {
+      ...state,
+      loading: true,
+      error: '',
+    };
+  }
+  if (action.type === 'success') {
+    return {
+      ...state,
+      loading: false,
+      error: '',
+      registered: true,
+    };
+  }
+  if (action.type === 'error') {
+    return {
+      ...state,
+      loading: false,
+      error: action.error,
+    };
+  }
+  if (action.type === 'input') {
+    return {
+      ...state,
+      [action.name]: action.value,
+    };
+  }
+  throw new Error(`That action doesn't exist`);
+}
+
+const SignIn = () => {
   const classes = useStyles();
+
+  const [state, dispatch] = React.useReducer(registerReducer, {
+    email: '',
+    password: '',
+    loading: false,
+    error: '',
+    registered: false,
+  });
+
+  // const handleSubmit = e => {
+  //   e.preventDefault();
+
+  //   dispatch({ type: 'login' });
+
+  //   newUser({
+  //     email: state.email,
+  //     password: state.password,
+  //   })
+  //     .then(() => dispatch({ type: 'success' }))
+  //     .catch(error =>
+  //       dispatch({
+  //         type: 'error',
+  //         error,
+  //       }),
+  //     );
+  // };
+
+  if (state.registered === true) {
+    return <Redirect to="/dashboard" />;
+  }
+
+  if (state.loading === true) {
+    return <Loading />;
+  }
 
   return (
     <Container component="main" maxWidth="xs">
       <CssBaseline />
       <div className={classes.paper}>
+        {state.error && <p>{state.error}</p>}
         <Typography component="h1" variant="h5">
           Sign in
         </Typography>
@@ -52,19 +127,27 @@ export default function SignIn() {
             className={classes.textfield}
             variant="outlined"
             margin="normal"
-            required
             fullWidth
             id="email"
             label="E-mail"
             name="email"
             autoComplete="email"
             autoFocus
+            onChange={
+              e =>
+                dispatch({
+                  type: 'input',
+                  name: 'email',
+                  value: e.target.value,
+                })
+              // eslint-disable-next-line react/jsx-curly-newline
+            }
+            value={state.email}
           />
           <InputLabel>Password:</InputLabel>
           <TextField
             variant="outlined"
             margin="normal"
-            required
             fullWidth
             name="password"
             label="Password"
@@ -95,4 +178,6 @@ export default function SignIn() {
       </div>
     </Container>
   );
-}
+};
+
+export default SignIn;
