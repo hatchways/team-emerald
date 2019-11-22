84ec02012c40d71d4996e73fdea108ec783a8b49
Victor <>
2019-11-21 17:11:09 -0800

    Work on add url bar

--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -1,26 +1,36 @@
 import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Box from '@material-ui/core/Box';
+import Container from '@material-ui/core/Container';
 import InputBase from '@material-ui/core/InputBase';
-import IconButton from '@material-ui/core/IconButton';
-import FormSubmitButton from './FormSubmitButton';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+import { FormGroup } from '@material-ui/core';
 
-const useStyles = makeStyles({
-  root: {
-    padding: 0,
+const useStyles = makeStyles(theme => ({
+  container: {
     display: 'flex',
-    justifyContent: 'center',
+    flexWrap: 'wrap',
+    justifyContent: 'center'
   },
   input: {
+    maxWidth: 260,
     backgroundColor: 'white',
     color: '#333',
-    margin: '50px 0',
-    padding: 12,
-    width: 300,
-    borderRadius: '22px 0 0 22px',
+    fontSize: theme.typography.fontSize * 1.5,
+    padding: '12px 24px',
+    borderRadius: '25px 0 0 25px',
     boxShadow: '0px 2px 6px #ccf',
   },
-});
+  root: {
+    maxWidth: 100,
+    backgroundColor: 'white',
+    color: '#333',
+    fontSize: theme.typography.fontSize * 1.5,
+    padding: '12px 24px',
+    borderRadius: '0 25px 25px 0',
+    boxShadow: '0px 2px 6px #ccf',
+  }
+}));
 
 export default function SearchBarprops() {
   const classes = useStyles();
@@ -31,15 +41,18 @@ export default function SearchBarprops() {
   }
 
   return (
-    <form>
-      <Box className={classes.root}>
-        <InputBase
-          className={classes.input}
-          placeholder="Search"
-          value={text}
-          onChange={handleInputChange}
-        />
-      </Box>
+      <form className={classes.container}>
+        <InputBase fullWidth className={classes.input}/>
+        <Select
+          class={classes.root}
+        >
+          <MenuItem value="">
+            None
+          </MenuItem>
+          <MenuItem value={10}>Ten</MenuItem>
+          <MenuItem value={20}>Twenty</MenuItem>
+          <MenuItem value={30}>Thirty</MenuItem>
+        </Select>
     </form>
   );
 }
