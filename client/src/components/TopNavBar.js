7b206f3706fa05c940e0befacf5ac84053735f00
Simon Mai <maismin@gmail.com>
2019-11-24 11:49:14 -0500

    Refactor TopNavBar
    - Refactor imports to reduce redundancy

--- client/src/components/TopNavBar.js
+++ client/src/components/TopNavBar.js
@@ -1,18 +1,21 @@
 import React, { useState } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Breadcrumbs from '@material-ui/core/Breadcrumbs';
-import Button from '@material-ui/core/Button';
-import Badge from '@material-ui/core/Badge';
-import Typography from '@material-ui/core/Typography';
 import { NavLink } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+import {
+  AppBar,
+  Badge,
+  Breadcrumbs,
+  Button,
+  Toolbar,
+  Typography,
+} from '@material-ui/core';
+
 import logo from '../images/logo.png';
 
 const useStyles = makeStyles(theme => ({
   appBar: {
     backgroundColor: 'white',
-    boxShadow: '0px 2px 6px #ccf',
+    boxShadow: theme.boxShadowTheme,
   },
   toolBar: {
     display: 'flex',
