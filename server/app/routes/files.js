6130a1d3a39f4791da268efb81ed2c1c2d4695f5
Victor <>
2019-11-24 08:59:58 -0800

    Uncomment middleware in routes/files.js

--- server/app/routes/files.js
+++ server/app/routes/files.js
@@ -2,11 +2,11 @@ const express = require('express');
 const expressFileUpload = require('express-fileupload');
 
 const { upload } = require('../controllers/files');
-// const { protect } = require('../middleware/auth');
+const { protect } = require('../middleware/auth');
 
 const router = express.Router();
 
 router.use(expressFileUpload());
-router.post('/upload', upload);
+router.post('/upload', protect, upload);
 
 module.exports = router;
