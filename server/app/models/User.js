7d05ddd925f09ab78c1e917cc02af2c06847163a
Simon Mai <maismin@gmail.com>
2019-11-26 19:35:41 -0500

    Fix User model to return its id with JSON response

--- server/app/models/User.js
+++ server/app/models/User.js
@@ -40,7 +40,7 @@ const UserSchema = mongoose.Schema(
 // to a JSON object
 UserSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    // returnedObject.id = returnedObject._id.toString();
+    returnedObject.id = returnedObject._id.toString();
     delete returnedObject._id;
     delete returnedObject.__v;
     delete returnedObject.createdAt;
