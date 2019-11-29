001e5718bf0a838f0150088775ba6f9b9ac82d23
Simon Mai <maismin@gmail.com>
2019-11-28 20:37:32 -0500

    Fix message queue
    - The two message queues are now reduced to one. The message queue for
    updating models is replaced with an event listener.
    
    - Add a script to start up the web scraping service

--- server/app/services/web-scraper/wss.js
+++ server/app/services/web-scraper/wss.js
@@ -1,9 +1,11 @@
 const Queue = require('bull');
 const currency = require('currency.js');
 
+const amazon = require('./amazon-web-scraper');
 const Product = require('../../models/Product');
 
-/* For testing purposes: REMOVE LATER when putting this file inside the products controller */
+/* **************************************************************************************** */
+// For testing purposes: REMOVE LATER when putting this file inside the products controller
 // eslint-disable-next-line import/order
 const dotenv = require('dotenv');
 
@@ -15,23 +17,11 @@ dotenv.config({
 });
 
 connectDB();
-/* ***************************************************************************************** */
+/* **************************************************************************************** */
 
-const sendQueue = new Queue('web-scrape-service');
-const receiveQueue = new Queue('update-product');
+const webScrapeQueue = new Queue('web-scrape-service');
 
-// Used for when users add new product links to database
-const newProductOptions = {
-  priority: 1,
-};
-
-// Used to webscrape existing products in database at a later time
-const delayOptions = {
-  priority: 10,
-  delay: 20000, // Change to a longer duration later
-};
-
-/*  FOR JOBS SENT ON THE WEB SCRAPE SERVICE QUEUE
+/*  FOR JOBS RECEIVED ON THE WEB SCRAPE SERVICE QUEUE
     Data for new products:
     data: {
       link: LINK_TO_PRODUCT_PAGE,
@@ -46,23 +36,34 @@ const delayOptions = {
     }
 */
 
-/* FOR JOBS RECEIVED ON THE UPDATE PRODUCT QUEUE
-    data: {
-      id: job.data.id || false,
-      productDetails: {
-        name:
-        link:
-        currentPrice:
-        previousPrice:
-        imageUrl:
-      }
-      newProduct: job.data.newProduct || false
-    }
-*/
+// Used for when users add new products to database
+const newProductOptions = {
+  priority: 1,
+};
 
-receiveQueue.process(async job => {
-  const { productDetails } = job.data;
+// Used to webscrape existing products in database at a later time
+const delayOptions = {
+  priority: 10,
+  delay: 20000, // Change to a longer duration later
+};
 
+webScrapeQueue.process(async job => {
+  // web-scrape link for product details
+  const productDetails = await amazon.getProductDetails(job.data.link);
+
+  const result = {
+    id: job.data.id,
+    productDetails,
+    newProduct: job.data.newProduct,
+  };
+
+  return result;
+});
+
+webScrapeQueue.on('completed', async (job, result) => {
+  const { productDetails } = result;
+
+  // convert currency from string to number
   productDetails.currentPrice = currency(productDetails.currentPrice).value;
   productDetails.previousPrice = currency(productDetails.previousPrice).value;
 
@@ -74,7 +75,7 @@ receiveQueue.process(async job => {
     let product = await Product.create(productDetails);
     product = product.toJSON();
 
-    sendQueue.add(
+    webScrapeQueue.add(
       {
         id: product.id,
         link: product.link,
@@ -112,31 +113,45 @@ receiveQueue.process(async job => {
       }
 
       console.log(`Fetching product at a later time...`);
-      sendQueue.add(
+      webScrapeQueue.add(
         {
           id: job.data.id,
-          link: savedProduct.link,
-          currentPrice: savedProduct.currentPrice,
+          link: savedProduct ? savedProduct.link : productInDb.link,
+          currentPrice: savedProduct
+            ? savedProduct.currentPrice
+            : productInDb.currentPrice,
         },
         delayOptions,
       );
     } catch (error) {
+      console.log(error);
       console.log('Product was not found in the database, not added to queue');
     }
   }
 });
 
-const urls = [
-  'https://www.amazon.com/Acer-Lightweight-i7-8565U-Back-lit-Keyboard/dp/B07JLBJZD3?smid=ATVPDKIKX0DER&pf_rd_p=76f0bb6a-acae-4563-a011-9d48589bd6bb&pf_rd_r=9CZQNXVGAK73FVAM482P',
-  // 'https://www.amazon.com/Legacy-Heating-CDFP-S-CB-M-Aluminum-Table/dp/B076GYTW6P?ref_=Oct_DLandingS_D_e7e1167a_63&smid=ATVPDKIKX0DER',
-];
+/* **************************************************************************************** */
+/* TEST */
+const test = () => {
+  const urls = [
+    'https://www.amazon.com/Acer-Lightweight-i7-8565U-Back-lit-Keyboard/dp/B07JLBJZD3?smid=ATVPDKIKX0DER&pf_rd_p=76f0bb6a-acae-4563-a011-9d48589bd6bb&pf_rd_r=9CZQNXVGAK73FVAM482P',
+    // 'https://www.amazon.com/Legacy-Heating-CDFP-S-CB-M-Aluminum-Table/dp/B076GYTW6P?ref_=Oct_DLandingS_D_e7e1167a_63&smid=ATVPDKIKX0DER',
+  ];
 
-urls.forEach(url => {
-  sendQueue.add(
-    {
-      link: url,
-      newProduct: true,
-    },
-    newProductOptions,
-  );
-});
+  urls.forEach(url => {
+    webScrapeQueue.add(
+      {
+        link: url,
+        newProduct: true,
+      },
+      newProductOptions,
+    );
+  });
+};
+
+// Start puppeteer
+(async () => {
+  await amazon.initialize();
+  test();
+})();
+/* **************************************************************************************** */
