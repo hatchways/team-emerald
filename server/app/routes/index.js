cc2b4c0f00003aeb4129736081c39e1461f2f996
Simon Mai <maismin@gmail.com>
2019-12-08 08:12:03 -0500

    Fix index.js route

--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -1,16 +1,14 @@
 const express = require('express');
 
 const authRouter = require('./auth');
-const listsRouter = require('./lists');
 const followsRouter = require('./follows');
 const listsRouter = require('./lists');
-const usersRouter = require('./users');
 const productsRouter = require('./products');
+const usersRouter = require('./users');
 
 const router = express();
 
 router.use('/auth', authRouter);
-router.use('/lists', listsRouter);
 router.use('/follows', followsRouter);
 router.use('/lists', listsRouter);
 router.use('/users', usersRouter);
