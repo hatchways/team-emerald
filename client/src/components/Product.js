67d5fb8c29e501ad539d5727e68dcb5ae0a48363
Simon Mai <maismin@gmail.com>
2019-12-09 08:49:12 -0500

    Fix Product component
    - Add currency.js dependency to format prices.
    
    - Set object-fit property to cover to maintain aspect ratio for images.
    
    - Change previousPrice to be optional. If the previousPrice is missing,
    it is not displayed in the browser.
    
    - Abbreviate product name.

--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -1,6 +1,7 @@
 /* eslint-disable react/jsx-one-expression-per-line */
 import React from 'react';
 import PropTypes from 'prop-types';
+import currency from 'currency.js';
 import { Box, Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
@@ -19,6 +20,7 @@ const useStyles = makeStyles(theme => ({
     minWidth: '11rem',
     maxWidth: '11rem',
     margin: '10px',
+    objectFit: 'contain',
   },
   info: {
     alignSelf: 'center',
@@ -41,23 +43,34 @@ const useStyles = makeStyles(theme => ({
 function Product(props) {
   const classes = useStyles(props);
   const { name, link, imgUrl, currentPrice, previousPrice } = props;
+  const abbreviatedName = `${name.slice(0, 78)}...`;
   const abbreviatedLink = `${link.slice(0, 48)}...`;
+
   return (
     <Box className={classes.box}>
       <Box>
         <img src={imgUrl} alt="ProductImage" className={classes.image} />
       </Box>
       <Box className={classes.info}>
-        <Typography variant="h6">{name}</Typography>
+        <Typography variant="h6">
+          {name.length > 78 ? abbreviatedName : name}
+        </Typography>
         <Typography className={classes.link} variant="body2">
           {link.length > 48 ? abbreviatedLink : link}
         </Typography>
         <Box>
-          <Typography display="inline" className={classes.previousPrice}>
-            ${previousPrice}
-          </Typography>
+          {previousPrice ? (
+            <Typography display="inline">
+              <Box component="span" className={classes.previousPrice}>
+                {currency(previousPrice, { formatWithSymbol: true }).format()}
+              </Box>
+              &nbsp;
+            </Typography>
+          ) : (
+            ''
+          )}
           <Typography display="inline" className={classes.currentPrice}>
-            &nbsp;&nbsp; ${currentPrice}
+            {currency(currentPrice, { formatWithSymbol: true }).format()}
           </Typography>
         </Box>
       </Box>
@@ -65,12 +78,16 @@ function Product(props) {
   );
 }
 
+Product.defaultProps = {
+  previousPrice: 0,
+};
+
 Product.propTypes = {
   name: PropTypes.string.isRequired,
   link: PropTypes.string.isRequired,
   imgUrl: PropTypes.string.isRequired,
   currentPrice: PropTypes.number.isRequired,
-  previousPrice: PropTypes.number.isRequired,
+  previousPrice: PropTypes.number,
 };
 
 export default Product;
